9404781091cca665a942c1068af142344138093e
aakash123254 <aakashharwani06@gmail.com>
2025-10-04 15:07:51 +0530

    Updated Code

--- app.py
+++ app.py
@@ -39,4 +39,64 @@ def yt_loader(yt_url):
     for i in range(len(transcript)):
         doc = Document(page_content=transcript[i].page_content)
         docs.append(doc)
-        
+    
+    yield "Splitting transcript into chunks of text..."
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        model_name = "gpt-3.5-turbo",
+        chunk_size=1000,
+        chunk_overlap=64,
+        separators=["\n\n", "\n", " "],
+    )
+    docs_splitter = text_splitter.split_documents(docs)
+    cohere_embeddings = CohereEmbeddings(model="large",cohere_api_key=COHERE_API_KEY)
+    
+    yield "Uploading chunks of text into Qdrant..."
+    qdrant = Qdrant.from_documents(
+        docs_splitter,
+        cohere_embeddings,
+        url = QDRANT_CLUSTER_URL,
+        prefer_grpc = True,
+        api_key = QDRANT_API_KEY,
+        collection_name = QDRANT_COLLECTION_NAME
+    )    
+    
+    with open(prompt_file,"r") as file:
+        prompt_template = file.read()
+    
+    PROMPT = PromptTemplate(
+        template = prompt_template,input_variables=["question","context"]
+    )
+    
+    llm = ChatOpenAI(
+        model_name = "gpt-3.5-turbo", temperature=0, openai_api_key = OPEN_API_KEY   
+    )
+    global qa 
+    qa = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retiever = qdrant.as_retriever(),
+        chain_type_kwargs = {"prompt":PROMPT},
+    )
+    
+    yield "Generating summarized text from transcript..."
+    chain = load_summarize_chain(llm = llm, chain_type="map_reduce")
+    summarize_text = chain.run(docs_splitter)
+    res = (
+        "Video ID:"
+        + video_id
+        + "\n"
+        + "Video Title: "
+        + title 
+        + "\n"
+        + "Channel Name: "
+        + author 
+        + "\n"
+        + "Summarize Text:"
+        +summarize_text
+    )
+    yield res 
+
+def chat(chat_history,query):
+    res = qa.run(query)
+    progressive_response = ""
+    
